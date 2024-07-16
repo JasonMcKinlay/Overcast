efe5d8f581e7d6a29f6797a92087e5bbedee04e3
JasonMcKinlay <70662170+JasonMcKinlay@users.noreply.github.com>
2024-07-15 23:41:09 -0500

    started front-end implementation

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
 
-const app = experss();
+const app = express();
 const port = 3000;
+
+app.get("/", async (req, res) => {
+    res.render("index.ejs");
+});
 
 app.listen(port, (req, res) => { console.log(`Server running on port ${port}.`) });
