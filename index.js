c196dcc89fc66247ecc01bfeb8ec23d90fbaee31
JasonMcKinlay <jasonmckinlay.us@gmail.com>
2025-01-19 21:10:42 -0500

    bodyParser for input

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true}));
+
 app.get("/", async (req, res) => {
     res.render("index.ejs");
 });
+
+app.post("/", async (req, res) => {
+    console.log(req.body.cityInput);
+    res.render("index.ejs")
+})
 
 app.listen(port, (req, res) => { console.log(`Server running on port ${port}.`) });
