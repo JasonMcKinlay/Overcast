951549c0e9d1ae4a7c30fc95d5acfb85305bad87
JasonMcKinlay <jasonmckinlay.us@gmail.com>
2025-01-21 11:24:16 -0500

    implemented axios requests

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
+import axios from "axios";
 import "dotenv/config";
 
 const app = express();
 const port = 3000;
 const API_KEY = process.env.API_KEY;
+
 
 app.use(bodyParser.urlencoded({ extended: true}));
 
@@ -14,6 +16,11 @@ app.get("/", async (req, res) => {
 
 app.post("/", async (req, res) => {
     console.log(req.body.cityInput);
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${req.body.cityInput}&appid=${API_KEY}&units=imperial`;
+    axios.get(url)
+    .then(function (response) {
+        console.log(response.data);
+    });
     res.render("index.ejs")
 })
 
