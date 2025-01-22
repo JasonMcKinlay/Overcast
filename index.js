a70a573fbbd7070f6a8a5040a7bd91ce62f04dc9
JasonMcKinlay <jasonmckinlay.us@gmail.com>
2025-01-22 15:13:34 -0500

    Update index.js
    backup before installing react

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import axios from "axios";
 import "dotenv/config";
 
+//const form = document.querySelector(".top-banner form");
 const app = express();
 const port = 3000;
 const API_KEY = process.env.API_KEY;
@@ -20,7 +21,34 @@ app.post("/", async (req, res) => {
     axios.get(url)
     .then(function (response) {
         console.log(response.data);
+        const city = response.data.name;
+        const country = response.data.sys.country;
+        const temp = response.data.main.temp;
+        const feelsLike = response.data.main.feelsLike;
+        const windSpeed = response.data.wind.speed;
+        const icon = `https://openweathermap.org/img/wn/${
+            response.data.weather.icon
+          }@2x.png`;
+        const description = response.data.weather.description;
     });
+
+    // const li = document.createElement("li");
+    // li.classList.add("city");
+    // const markup = ` 
+    // <h2 class="city-name" data-name="${city},${country}"> 
+    // <span>${city}</span> 
+    // <sup>${country}</sup> 
+    // </h2> 
+    // <div class="city-temp">${Math.round(temp)}<sup>°C</sup> 
+    // </div> 
+    // <figure> 
+    // <img class="city-icon" src=${icon} alt=${description}> 
+    // <figcaption>${description}</figcaption> 
+    // </figure> 
+    // `;
+    // li.innerHTML = markup;
+    // list.appendChild(li);
+
     res.render("index.ejs")
 })
 
