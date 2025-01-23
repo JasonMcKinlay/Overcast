6e023f9591a05b0916d91869f721e550c917edd0
JasonMcKinlay <jasonmckinlay.us@gmail.com>
2025-01-23 10:36:07 -0500

    installed react
    WIP, not displaying correctly

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+    return (
+        <div>Weather App</div>
+    )
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+export default {
+  entry: "./src/index.jsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+};
