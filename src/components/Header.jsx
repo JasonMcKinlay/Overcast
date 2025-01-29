cc74b1e3f323a56c6c352fced402a9c7cd4786b7
JasonMcKinlay <jasonmckinlay.us@gmail.com>
2025-01-29 15:13:20 -0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,14 @@ import React from "react";
 function Header() {
     return (
         <div>
-            <div class="py-5 mx-5 my-5 text-auto col-lg-6 align-items-center justify-content-center">
-                <h1 class="heading display-5 fw-bold text-body-emphasis text-center">Let's check the weather.</h1>
-                    <form class="d-flex align-items-center justify-content-center" method="POST">
-                        <div class="input-group" style="width: 50%">
-                            <input type="text" placeholder="Search for a city" class="form-control form-control-dark text-bg-dark col-lg-6" name="cityInput" autofocus />
-                            <button type="submit" class="btn btn-outline-primary px-4">Submit</button>
+            <div className="py-5 mx-5 my-5 text-auto col-lg-6 align-items-center justify-content-center">
+                <h1 className="heading display-5 fw-bold text-body-emphasis text-center">Let's check the weather.</h1>
+                    <form className="d-flex align-items-center justify-content-center" method="POST">
+                        <div className="input-group" style={{width: "50%"}}>
+                            <input type="text" placeholder="Search for a city" className="form-control form-control-dark text-bg-dark col-lg-6" name="cityInput" />
+                            <button type="submit" className="btn btn-outline-primary px-4">Submit</button>
                         </div>
-                        <span class="msg ms-3"></span>
+                        <span className="msg ms-3"></span>
                     </form>
           
             </div>
