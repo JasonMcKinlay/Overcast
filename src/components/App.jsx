7d8106a86fe030ae30e63ea51919e749fd18b50d
JasonMcKinlay <jasonmckinlay.us@gmail.com>
2025-01-30 20:35:49 -0500

    Created Card.jsx
    added dark mode to React app, fixed minor details in Header.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Header from "./Header.jsx";
+import Card from "./Card.jsx";
 
 function App() {
     return (
         <div>
             <Header />
+            <ul className="cities row justify-content-center">
+                <Card />
+                <Card />
+                <Card />
+            </ul>
         </div>
     );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Header() {
     return (
-        <div>
+        <div className="top-banner container d-flex justify-content-center">
             <div className="py-5 mx-5 my-5 text-auto col-lg-6 align-items-center justify-content-center">
                 <h1 className="heading display-5 fw-bold text-body-emphasis text-center">Let's check the weather.</h1>
                     <form className="d-flex align-items-center justify-content-center" method="POST">
