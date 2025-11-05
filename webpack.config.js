1ac9cdd5d3891b60f466e47a1dbd5367c145ca94
JasonMcKinlay <jasonmckinlay.us@gmail.com>
2025-11-04 19:46:24 -0500

    updated webpack.config.js
    created fixes to production and development modes, allowing development mode to work properly locally

--- webpack.config.js
+++ webpack.config.js
@@ -10,55 +10,59 @@ dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export default {
-  entry: "./src/index.js", 
-  output: {
-    path: path.resolve(__dirname, "build"), 
-    filename: "bundle.js",
-    publicPath: "/Overcast/",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/, 
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-react"], 
-          },
-        },
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-    new webpack.DefinePlugin({
-      'process.env.REACT_APP_API_KEY': JSON.stringify(process.env.REACT_APP_API_KEY),
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        { 
-          from: "public",
-          globOptions: {
-            ignore: ["**/index.html"]
+export default (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  
+  return {
+    entry: "./src/index.js", 
+    output: {
+      path: path.resolve(__dirname, "build"), 
+      filename: "bundle.js",
+      publicPath: isProduction ? "/Overcast/" : "/",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/, 
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-react"], 
+            },
           },
         },
       ],
-    }),
-  ],
-  resolve: {
-    extensions: [".js", ".jsx"],
-  },
-  mode: "production",
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, "public"), // Only serve from the public directory
     },
-    port: 8080, 
-    hot: true, 
-    open: true, 
-  },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+      }),
+      new webpack.DefinePlugin({
+        'process.env.REACT_APP_API_KEY': JSON.stringify(process.env.REACT_APP_API_KEY),
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          { 
+            from: "public",
+            globOptions: {
+              ignore: ["**/index.html"]
+            },
+          },
+        ],
+      }),
+    ],
+    resolve: {
+      extensions: [".js", ".jsx"],
+    },
+    mode: isProduction ? "production" : "development",
+    devServer: {
+      static: {
+        directory: path.resolve(__dirname, "public"),
+      },
+      port: 3000, 
+      hot: true, 
+      open: true, 
+    },
+  };
 };
